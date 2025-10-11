e14617c38c4e9817487075836db5c3f42a08a488
abdullah <164213606+abdull435@users.noreply.github.com>
2025-10-11 22:01:44 +0500

    add heading and sub heading below button header

--- swift_trip/lib/screen/custom_tour.dart
+++ swift_trip/lib/screen/custom_tour.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 
-class CustomTour extends StatelessWidget{
+class CustomTour extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text("Custom Tour Creation")),
-      ),
-      body:  SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+      appBar: AppBar(title: Center(child: Text("Custom Tour Creation"))),
+      body: SafeArea(
+        child: Column(
           children: [
-            Text("Destination"),
-            Text("Agency"),
-            Text("Planning"),
-            Text("Summary"),
-            Text("Payment")
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Destination"),
+                  Text("Agency"),
+                  Text("Planning"),
+                  Text("Summary"),
+                  Text("Payment"),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("Select Your Journey"),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text("Choose your departure and arrival cities"),
+                ),
+              ],
+            ),
           ],
         ),
       ),
