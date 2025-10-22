aad554649abd7ff7cfb634941ca0e1e970f10546
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-10-22 23:04:49 +0500

    use Appbar widget and set as appbar here

--- swift_trip/lib/screen/payment_screen.dart
+++ swift_trip/lib/screen/payment_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:swift_trip/screen/AppBar.dart';
 
 class PaymentScreen extends StatefulWidget {
   const PaymentScreen({super.key});
@@ -9,10 +10,15 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreenState extends State<PaymentScreen> {
+  int selectedIndex = 4;
+
   @override
   Widget build(context) {
     return Scaffold(
-      
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: Appbar(selectedIndex: selectedIndex),
+      ),
     );
   }
 }
