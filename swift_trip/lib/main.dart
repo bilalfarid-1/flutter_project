daa812027fea4292ed729c64a5977391443f84d1
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-11-13 17:41:22 +0500

    Initialize Firebase in main.dart
    Added Firebase initialization to the app startup sequence in main.dart. Ensures WidgetsFlutterBinding is initialized before calling Firebase.initializeApp(), and handles initialization errors with a print statement.

--- swift_trip/lib/main.dart
+++ swift_trip/lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_trip/screen/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Firebase initialization failed: $e');
+  }
+
   runApp(
-     MaterialApp(
+    MaterialApp(
       title: 'Tourist App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: Color(0xFFDFF2FE),
-          ),
-      home:  LoginScreen(),
-      ),
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xFFDFF2FE)),
+      home: LoginScreen(),
+    ),
   );
 }
