8016e2ecb6414fba6fdb7a882e53a1d5d37ed3b6
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-11-15 18:27:00 +0500

    Refactor Firebase initialization for web support in main.dart

--- swift_trip/lib/main.dart
+++ swift_trip/lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_trip/screen/login_screen.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   try {
-    await Firebase.initializeApp();
-  } catch (e) {
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: 'AIzaSyAbOuPoknoeCX0tor56aWcI0-p0_uIoDwI',
+          authDomain: 'madproject-6891b.firebaseapp.com',
+          projectId: 'madproject-6891b',
+          storageBucket: 'madproject-6891b.firebasestorage.app',
+          messagingSenderId: '736157463174',
+          appId: '1:736157463174:android:66c8f463f5edd74d69d707',
+        ),
+      );
+    } else {
+      await Firebase.initializeApp();
+    }
+
+    print('firebase is running');
+  } catch (e, stack) {
     print('Firebase initialization failed: $e');
+    print(stack);
   }
 
   runApp(
