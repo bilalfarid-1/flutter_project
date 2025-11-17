8b6522c40bdbe2f3a2b690cf823dffc708abee33
abdullah <164213606+abdull435@users.noreply.github.com>
2025-11-17 22:00:00 +0500

    remove unnecssary imports

--- swift_trip/lib/main.dart
+++ swift_trip/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_trip/screen/login_screen.dart';
-import 'package:swift_trip/screen/signup_screen.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
