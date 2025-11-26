b337347a54153e4a554c51cb4d8a77e042eb2d6c
abdullah <164213606+abdull435@users.noreply.github.com>
2025-11-26 13:30:00 +0500

    remove unused screens and components from the project

--- swift_trip/lib/main.dart
+++ swift_trip/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:swift_trip/screen/destination.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:swift_trip/screen/signup_screen.dart';
 
